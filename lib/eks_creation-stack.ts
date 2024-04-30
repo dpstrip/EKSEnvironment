a87f60e6e2c524de3575a71a11133115a64bfe58
David Stripeik <dpstrip@gmail.com>
2024-04-30 16:29:09 -0500

    updated to include iam roles

--- lib/eks_creation-stack.ts
+++ lib/eks_creation-stack.ts
@@ -1,16 +1,24 @@
 import * as cdk from 'aws-cdk-lib';
 import { Construct } from 'constructs';
-// import * as sqs from 'aws-cdk-lib/aws-sqs';
+import * as s3 from 'aws-cdk-lib/aws-s3';
+import {PrivateCluster} from './cluster';
+import {aws_ec2 as ec2} from 'aws-cdk-lib';
 
 export class EksCreationStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    new s3.Bucket(this, 'AS3Bucket',{
+      versioned:true,
+    })
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'EksCreationQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
-  }
+
+  //Get the curnet vpc
+    const vpc = ec2.Vpc.fromLookup(this, 'VPC',{
+      vpcId:"vpc-0e7a7fc9ede3b6bbb",
+    })
+
+  //create EKS cluster
+  new PrivateCluster(this, 'dpsCluster', vpc).cluster;
+};
 }
